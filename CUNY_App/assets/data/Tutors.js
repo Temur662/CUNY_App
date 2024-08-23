0a7421e260c0e6cd2121789f53a751efc30d01ae
Abubeckr Elcharfa <abubeckr39@gmail.com>
2024-08-22 20:22:53 -0400

    add brackets to the data sets

--- CUNY_App/assets/data/Tutors.js
+++ CUNY_App/assets/data/Tutors.js
@@ -32,6 +32,7 @@ export const Tutors = [
       "subject": "Physics",
       "backdrop" : 'https://as2.ftcdn.net/v2/jpg/03/30/28/71/1000_F_330287153_5cSfp71gxln3S6Okn6ofKmEfK1khhlzd.jpg',
       "tutors": [
+        {},
         {
           "name": "Tina Gonzales",
           "classes_covered": ["Physics 101" ,"Physics 102", "Physics 203"],
@@ -52,13 +53,15 @@ export const Tutors = [
           "availability": ["Wednesday, 11:00 AM - 3:00 PM"],
           "location": "6S Room 115",
           "contact": "lmorales@csi.cuny.edu"
-        }
+        },
+        {}
       ]
     },
     {
       "subject": "Chemistry",
       "backdrop" : "https://img.freepik.com/free-vector/science-scribbles_23-2147501583.jpg?t=st=1724355442~exp=1724359042~hmac=90fca8afd9bd78d6b00c4909f10410647b51f5d7eddcb16b8d646a26eb9d5b01&w=1800",
       "tutors": [
+        {},
         {
           "name": "Dr. Samuel Green",
           "classes_covered": ["General Chemistry", "Organic Chemistry"],
@@ -79,13 +82,15 @@ export const Tutors = [
           "availability": ["Wednesday 1:00 PM - 4:00 PM","Friday, 1:00 PM - 4:00 PM"],
           "location": "6S Room 125",
           "contact": "mwhite@csi.cuny.edu"
-        }
+        },
+        {}
       ]
     },
     {
       "subject": "Computer Science",
       "backdrop" : 'https://img.freepik.com/free-vector/neon-circuit-board-background_23-2148340911.jpg?t=st=1724355608~exp=1724359208~hmac=55cd3b87e78524d7550bc5ebb5da6522b95b4e79b7ec99105ffad93c847085b0&w=2000',
       "tutors": [
+        {},
         {
           "name": "Daniel Carter",
           "classes_covered": ["Intro to Programming", "Data Structures", "Artificial Intelligence"],
@@ -106,13 +111,15 @@ export const Tutors = [
           "availability": ["Friday, 12:00 PM - 4:00 PM"],
           "location": "Computer Science Department (1P), Room 223",
           "contact": "jkim@csi.cuny.edu"
-        }
+        },
+        {}
       ]
     },
     {
       "subject": "Biology",
       "backdrop": 'https://plus.unsplash.com/premium_photo-1683121859548-b4514fa05e52?q=80&w=3498&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       "tutors": [
+        {},
         {
           "name": "Dr. Jessica Brown",
           "classes_covered": ["General Biology", "General Anatomy & Physiology"],
@@ -133,7 +140,8 @@ export const Tutors = [
           "availability": ["Friday, 10:00 AM - 2:00 PM"],
           "location": "6S Room 215",
           "contact": "slee@csi.cuny.edu"
-        }
+        },
+        {}
       ]
     }
   ]
